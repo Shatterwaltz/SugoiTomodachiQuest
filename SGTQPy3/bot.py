31369aa3c9c76d68cf958d05a6e9a9ac9c890f8d
Cory Sabol <sible777@gmail.com>
2017-04-24 04:52:49 -0400

    py bot handling messages, reformat help msg

--- SGTQPy3/bot.py
+++ SGTQPy3/bot.py
@@ -24,4 +24,18 @@ async def on_message(message):
         await asyncio.sleep(5)
         await client.send_message(message.channel, 'Done sleeping')
 
+    elif message.content.startswith('!help'):
+        hlp = """!stats - show your stats
+                 !survey - survey the current area
+                 !inv - show inventory
+                    |--> this should have sub commands
+                 !use <item/thing> - use an item or environment item
+                 !move <cardinal dir> - move your player
+                 !equip <item> - equip an item
+                 !throw - throw item in hand (make sure to equip thing you want to throw first
+                 !speak <item> - speak to something, can talk to innanimate things too, just might look silly
+                 !roll <die type> - roll a die, not used in game,
+                     but can be used by players to determine who should get to do something."""
+        await client.send_message(message.channel, hlp)
+
 client.run('MzA1OTM2NjMwMzgyOTg1MjE3.C98edg.9bPTOe4-aB5Xk0PLeWtAhxCILFo')
