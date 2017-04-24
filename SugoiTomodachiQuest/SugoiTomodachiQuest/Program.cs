9986083d17035f8feb999f9a7969823a25db9f00
Cory Sabol <sible777@gmail.com>
2017-04-24 01:51:08 -0400

    implementing async context

--- SugoiTomodachiQuest/SugoiTomodachiQuest/Program.cs
+++ SugoiTomodachiQuest/SugoiTomodachiQuest/Program.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
+using Discord.Logging;
 
 namespace SugoiTomodachiQuest {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync() {
         }
-    }
+
+        /*private Task Log(LogMessage msg) {
+            Console.WriteLine(msg.ToString());
+            Task.CompletedTask;
+        }*/
+   }
 }
+
