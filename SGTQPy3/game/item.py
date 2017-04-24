e86c652137629632f0de2203a3d248295f4b4b80
Cory Sabol <sible777@gmail.com>
2017-04-24 09:56:18 -0400

    add item class shell

---
+++ SGTQPy3/game/item.py
@@ -0,0 +1,13 @@
+
+class Item:
+	'''base item class. Other items can inherit this'''
+	
+	itype = ''
+	desc = ''
+	attribs = {} # match the stats dict of a character with values to apply as buffs?
+
+	def __init__():
+		None
+
+	def __del__():
+		None
