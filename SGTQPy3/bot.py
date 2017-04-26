7b975a0fb709d7b000089c7e0a723420730850b1
Cory Sabol <sible777@gmail.com>
2017-04-26 00:42:29 -0400

    can now roll die using RPN

--- SGTQPy3/bot.py
+++ SGTQPy3/bot.py
@@ -38,30 +38,35 @@ async def on_message(message):
         tmp = None
         roller = dieroller.DieRoller()
         sides = 0
-        try: 
-            sides = int(cmdargs[0][1:])
-        except ValueError:
-            sides = 20
+        print(cmdargs)
+        if 'd(' in cmdargs[0] and ')' in cmdargs[-1]:
+            print(' '.join(cmdargs))
+            expr = ' '.join(cmdargs)[2:-1]
+            print(expr)
+            sides = roller.reversepolish(expr)
+        else:
+            try: 
+                sides = int(cmdargs[0][1:])
+            except ValueError:
+                sides = 20
         num = roller.roll(sides)
         tmp = await client.send_message(message.channel, '`Rolling d{}`'.format(sides))
         await client.edit_message(tmp, '`You rolled a {}`'.format(num))
     creg.regcmd('!roll', rolldie)
 
     async def join(cmdargs):
-        tmp = await client.send_message(message.channel, '`Thinking...`')
-        print(message.author)
-        if message.author in players:
-            await client.edit_message(tmp, '`Found you, waking up character!`')
-        else:
-            players.append(message.author)
-            await client.edit_message(tmp, '`Looks like you don\'t exist yet... let\'s create you!`')
-            await client.send_message(message.channel, \
-                """```Choose ye class, nerd !class:
-                1: Wizardo
-                2: Sneaky fuck
-                3: Baldo
-                4: GymRat
-                5: Weeb```""")
+        if not message.channel.is_private:
+            await client.send_message(message.channel, '`You may join by sending me a dm: !join`')
+        elif message.channel.is_private:
+            tmp = await client.send_message(message.channel, '`Thinking...`')
+            print(message.author)
+            if message.author in players:
+                await client.edit_message(tmp, '`Found you, waking up character!`')
+            else:
+                players.append(message.author)
+                await client.edit_message(tmp, '`Looks like you don\'t exist yet... let\'s create you!`')
+                await client.send_message(message.channel, \
+                    'CLASSES HERE EVENTUALLY')
     creg.regcmd('!join', join)
 
     async def getplayers(cmdargs):
@@ -69,9 +74,18 @@ async def on_message(message):
         plyrs = ""
         for i in range(len(players)):
             plyrs = plyrs + str(players[i])
-        await client.edit_message(tmp, '`{}`'.format(plyrs))
+        await client.edit_message(tmp, '`{}`\n'.format(plyrs))
     creg.regcmd('!players', getplayers)
 
+    '''
+    async def testtyping(cmdargs):
+        tmp = await client.send_message(message.channel, 'Hey! ')
+        msg = 'I think you are great!'
+        for i in range(len(msg)):
+            await client.edit_message(tmp, tmp.content + msg[:i])
+    creg.regcmd('!testtype', testtyping)
+    # add the navy seal copy pasta
+    '''
     # handle the command string passed in
     if message.content.startswith('!'):
         await creg.callcmdasync(message.content)
