7ac2de921e220fc33f165527e33d78cb7f5ebb78
Cory Sabol <sible777@gmail.com>
2017-04-27 06:06:21 -0400

    retabbed character class

--- SGTQPy3/game/character.py
+++ SGTQPy3/game/character.py
@@ -1,32 +1,32 @@
 
 class Character:
-	'''Character class, NPCs, players, and mobs inherit this class'''
-        def __init__(self, desc, ctype, did):	
-            self.name = ''
-            self.desc = ''
-            self.ctype = '' # the type of character this is
-            self.did = 0 #discord id, 0 is for non player characters
-            self.pos = (0,0) # map room position
-            self.awake = False
-            
-            # these will probably change around. Just placeholder for now.
-            self.stats = {
-            	'hp' : 0,
-            	'str' : 0,
-            	'dex' : 0,
-            	'int' : 0,
-            	'wis' : 0,
-            	'atk' : 0,
-            	'def' : 0,
-            	'eva' : 0, 
-            	'lck' : 0
-            }
+    '''Character class, NPCs, players, and mobs inherit this class'''
+    def __init__(self, desc, ctype, did):	
+        self.name = ''
+        self.desc = ''
+        self.ctype = '' # the type of character this is
+        self.did = 0 #discord id, 0 is for non player characters
+        self.pos = (0,0) # map room position
+        self.awake = False
+        
+        # these will probably change around. Just placeholder for now.
+        self.stats = {
+            'hp' : 0,
+            'str' : 0,
+            'dex' : 0,
+            'int' : 0,
+            'wis' : 0,
+            'atk' : 0,
+            'def' : 0,
+            'eva' : 0, 
+            'lck' : 0
+        }
 
-	def setstats():
-		return None
+    def setstats():
+        return None
 
-	def getstats():
-		return stats
-
-	def __del__():
-		None
+    def getstats():
+        return stats
+    
+    def __del__():
+        None
