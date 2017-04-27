1d3cc75eff784dbbde949a80a16859e0ae2abb1e
Cory Sabol <sible777@gmail.com>
2017-04-27 06:03:16 -0400

    add GameSession and Player classes

---
+++ SGTQPy3/game/player.py
@@ -0,0 +1,9 @@
+
+class Player:
+    '''
+    Class that holds all info relevant to a player.
+    Their Discord username, their character and such.
+    '''
+    def __init__(self, name):
+        self.name = name
+        self.pcharacter = None
