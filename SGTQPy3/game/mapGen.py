9648cd68372b2a65a360a4aa1aa2bb879c5f76a4
Ben <shatterwaltz@gmail.com>
2017-04-27 22:27:45 -0400

    Completed?
    Discovered and fixed some big bugs.
    No longer crashes on creating dungeons smaller than 3x3.
    No longer crashes on creating dungeons that aren't perfectly square.
    
    Added third parameter to generation: targetRoomCount, allowing you to
    specify a number of rooms from 1 to width*height for the dungeon to
    contain.
    
    Successfully creates dungeons of size 1x1 and larger.
    Still need to make time optimizations, but it's completely functional
    now.

--- SGTQPy3/game/mapGen.py
+++ SGTQPy3/game/mapGen.py
@@ -1,62 +1,80 @@
 import random
 from room import Room
-class MapGen:
 
-	"""possibly some errors if you try to generate a map with width or height 1"""
-	def generate(self, width, height):
-		print("generating...")
-		"""init empty 2d array"""
-		map = [[None for w in range(width)] for h in range(height)]
-		"""Place room in center of map"""
-		map[int(height/2)][int(width/2)] = Room(int(height/2), int(width/2))
-		"""Loop will fill entire map with rooms."""
-		for i in range((width*height-1)):
-			"""possible locations to add a room are stored in this list and then randomly chosen"""
-			possibleRoomLocations = [] 
-			for w in range(width):
-				for h in range(height):
-					temp=self.ifEmptyGetAdjacent(w, h, map)
-					if temp!=None:
-						"""Add a tuple containing the coords of an empty space and an adjacent room"""
-						possibleRoomLocations.append((w, h, temp))
-			"""Grab a random option from list of possibilities"""
-			selection = random.choice(possibleRoomLocations)
-			"""init a room at selected location"""
-			newRoom = Room(selection[0], selection[1])
-			map[selection[1]][selection[0]] = newRoom
-			selection[2].addAdjacentRoom(newRoom)
-			newRoom.addAdjacentRoom(selection[2])
-		print("generation complete")
-		return map
-		
-	"""given coordinates, return an adjacent room if it coordinates are empty"""	
-	def ifEmptyGetAdjacent(self, x, y, map):
-		if map[y][x]==None:
-			if x>0:
-				if map[y][x-1] != None:
-					return map[y][x-1]
-			
-			if y>0:
-				if map[y-1][x] != None:
-					return map[y-1][x]
-			
-			if x<len(map[0])-2:
-				if map[y][x+1] != None:
-					return map[y][x+1]
-			
-			if y<len(map)-2:
-				if map[y+1][x] != None:
-					return map[y+1][x]
-					
-		return None	
+"""generate and printMap are the only two important ones. import these."""
+
+def generate(width, height, targetRoomCount):
+	w = width
+	h = height
+	if w < 1:
+		w = 1
+	if h < 1:
+		h = 1
+	if targetRoomCount < 1 or targetRoomCount > w*h:
+		targetRoomCount = w*h
+		print(targetRoomCount)
+	return generateHelper(w, h, targetRoomCount)
+
+def printMap(map):
+	ostring=""
+	for i in range(len(map[0])):
+		for j in range(len(map)):
+			if map[j][i]==None:
+				ostring+="|????"
+			else:
+				ostring+="|"+str(map[j][i].printAdj())
+		ostring+="|\n"	
+	return ostring	
+
 	
-	def printMap(self, map):
-		ostring=""
-		for i in range(len(map[0])):
-			for j in range(len(map)):
-				if map[j][i]==None:
-					ostring+="|????"
-				else:
-					ostring+="|"+str(map[i][j].printAdj())
-			ostring+="|\n"	
-		return ostring
+	
+"""Errors if you create a map too small, use generate() to be safe"""	
+def generateHelper(width, height, targetRoomCount):
+	print("generating...")
+	"""init empty 2d array"""
+	map = [[None for h in range(height)] for w in range(width)]
+	"""Place room in center of map"""
+	map[int(width/2)][int(height/2)] = Room(int(width/2), int(height/2))
+	"""Loop will fill entire map with rooms."""
+	for i in range((targetRoomCount-1)):
+		"""possible locations to add a room are stored in this list and then randomly chosen"""
+		possibleRoomLocations = [] 
+		for w in range(width):
+			for h in range(height):
+				temp=ifEmptyGetAdjacent(w, h, map)
+				if temp!=None:
+					"""Add a tuple containing the coords of an empty space and an adjacent room"""
+					possibleRoomLocations.append((w, h, temp))
+		"""Grab a random option from list of possibilities"""
+		selection = random.choice(possibleRoomLocations)
+		"""init a room at selected location"""
+		newRoom = Room(selection[0], selection[1])
+		map[selection[0]][selection[1]] = newRoom
+		selection[2].addAdjacentRoom(newRoom)
+		newRoom.addAdjacentRoom(selection[2])
+	print("generation complete")
+	return map
+	
+"""given coordinates, return an adjacent room if it coordinates are empty"""	
+def ifEmptyGetAdjacent(x, y, map):
+	if map[x][y]==None:
+		choices=[]
+		if x>0:
+			if map[x-1][y] != None:
+				choices.append(map[x-1][y])
+		if y>0:
+			if map[x][y-1] != None:
+				choices.append(map[x][y-1])
+		
+		if x<len(map)-1:
+			if map[x+1][y] != None:
+				choices.append(map[x+1][y])
+		
+		if y<len(map[0])-1:
+			if map[x][y+1] != None:
+				choices.append(map[x][y+1])
+				
+		if choices!=[]:
+			return random.choice(choices)		
+	return None	
+
