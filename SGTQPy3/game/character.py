de6f56f91a8fb577cc88bb4db8b734163d803e78
Cory Sabol <sible777@gmail.com>
2017-05-20 14:21:27 -0400

    moved stats to it's own separate class in game module

--- SGTQPy3/game/character.py
+++ SGTQPy3/game/character.py
@@ -1,121 +1,10 @@
-from lib import itermixin
+from game import stats
 
-class Character(itermixin.IterMixin):
+class Character(object):
     '''
-    Character class, NPCs, players, and mobs inherit this class
-    
-    STATS:
-        Health:     HP
-        Armor:      AR
-        Power:      PW
-        Evasion:    EV
-        Accuracy:   AC
-        Speed:      SP
-        Status:     ST
-        Luck:       LK
-
+    Class which represents a character in the game.
+    A character is anything which needs to have stats and a map position.
     '''
-
-    # stats subclass for custom setter logic
-    # each stat is a triple representing 
-    # (base,modified,modifier)
-    # This will be easy to pull apart anytime
-    # info about a stat is needed. 
-    class Stats(itermixin.IterMixin):
-        def __init__(self):
-            self.__health       = (0,0,0)
-            self.__armor        = (0,0,0)
-            self.__power        = (0,0,0)
-            self.__evasion      = (0,0,0)
-            self.__accuracy     = (0,0,0)
-            self.__speed        = (0,0,0)
-            self.__strength     = (0,0,0)
-            self.__luck         = (0,0,0)
-            self.__resitance    = (0,0,0)
-
-        @property
-        def health(self):
-            return self.__health
-        @health.setter
-        def health(self, val):
-            try:
-                self.__health = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def armor(self):
-            return self.__health
-        @health.setter
-        def armor(self, val):
-            try:
-                self.__health = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def power(self):
-            return self.__power
-        @power.setter
-        def power(self, val):
-            try:
-                self.__power = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def evasion(self):
-            return self.__evasion
-        @evasion.setter
-        def evasion(self, val):
-            try:
-                self.__evasion = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def accuracy(self):
-            return self.__accuracy
-        @accuracy.setter
-        def accuracy(self, val):
-            try:
-                self.__accuracy = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def speed(self):
-            return self.__speed
-        @speed.setter
-        def speed(self, val):
-            try:
-                self.__speed = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def strength(self):
-            return self.__strength
-        @strength.setter
-        def strength(self, val):
-            try:
-                self.__strength = int(val)
-            except ValueError:
-                pass
-
-        @property
-        def luck(self):
-            return self.__luck
-        @luck.setter
-        def luck(self, val):
-            try:
-                self.__luck = int(val)
-            except ValueError:
-                pass
-
-        def getallstats(self):
-            return None
-
     
     def __init__(self, desc, ctype, did):	
         self.name = ''
@@ -124,7 +13,7 @@ class Character(itermixin.IterMixin):
         self.did = 0 #discord id, 0 is for non player characters
         self.pos = (0,0) # map room position
         self.awake = False
-        self.__stats = self.Stats()
+        self.__stats = stats.Stats()
         
     @property
     def stats(self):
@@ -132,7 +21,7 @@ class Character(itermixin.IterMixin):
 
     @stats.setter
     def stats(self, val):
-        if val is self.Stats:
+        if val is stats.Stats:
             self.__state = val
         else:
             pass
